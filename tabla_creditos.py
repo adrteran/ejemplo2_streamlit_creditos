a1a0b5830349750bd71945a4a26b97b29f1bb4b5
Adrian Terán <aotl1989@gmail.com>
2025-10-22 14:55:53 -0500

    segunda actualizacion

--- tabla_creditos.py
+++ tabla_creditos.py
@@ -4,4 +4,9 @@ from mis_funciones import tasa, get_cuota, get_tabla
 st.set_page_config("Ejemplo2_Streamlit",page_icon="🤖")
 st.title("Tabla de amortizacion")
 
+monto=st.number_input("Ingrese el monto solicitado",min_values=0)
+plazo=st.number_input("Ingrese el numero de cuotas",min_values=0)
+
+
+
 
